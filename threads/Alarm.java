431d9ba54057c29630dc8f55e388355af6cc2816
Ashley Yung <nyung@ieng6-201.ucsd.edu>
2014-04-23 15:58:26 -0700

    Part 3 completed

--- threads/Alarm.java
+++ threads/Alarm.java
@@ -1,6 +1,7 @@
 package nachos.threads;
 
 import nachos.machine.*;
+import java.util.*;
 
 /**
  * Uses the hardware timer to provide preemption, and to allow threads to sleep
@@ -14,12 +15,15 @@ public class Alarm {
 	 * <p>
 	 * <b>Note</b>: Nachos will not function correctly with more than one alarm.
 	 */
+	private PriorityQueue<TimedThread> waitQueue;
+	
 	public Alarm() {
 		Machine.timer().setInterruptHandler(new Runnable() {
 			public void run() {
 				timerInterrupt();
 			}
 		});
+		waitQueue = new PriorityQueue<TimedThread>();
 	}
 
 	/**
@@ -29,7 +33,13 @@ public class Alarm {
 	 * should be run.
 	 */
 	public void timerInterrupt() {
-		KThread.currentThread().yield();
+		Machine.interrupt().disable();
+		while(!waitQueue.isEmpty() && waitQueue.peek().getWaitTime() <= Machine.timer().getTime()) {
+			TimedThread first = waitQueue.remove();
+			first.getKThread().ready();
+		}
+		Machine.interrupt().enable();
+		KThread.yield();
 	}
 
 	/**
@@ -47,7 +57,46 @@ public class Alarm {
 	public void waitUntil(long x) {
 		// for now, cheat just to get something working (busy waiting is bad)
 		long wakeTime = Machine.timer().getTime() + x;
-		while (wakeTime > Machine.timer().getTime())
-			KThread.yield();
+		Machine.interrupt().disable();
+		waitQueue.add(new TimedThread(wakeTime, KThread.currentThread()));
+		KThread.sleep();
+		Machine.interrupt().enable();
+	}
+	
+	private class TimedThread implements Comparable<TimedThread> {
+		protected Long waitTime;
+		private KThread kt;
+		
+		protected TimedThread(long waitTime, KThread kt) {
+			setWaitTime(waitTime);
+			setKThread(kt);
+		}
+		
+		@Override
+		public int compareTo(TimedThread tt) {
+			if(getWaitTime() > tt.getWaitTime())
+				return 1;
+			else if (getWaitTime() == tt.getWaitTime()) {
+				return 0;
+			}
+			else 
+				return -1;
+		}
+
+		public KThread getKThread() {
+			return kt;
+		}
+
+		public void setKThread(KThread kt) {
+			this.kt = kt;
+		}
+		
+		public long getWaitTime() {
+			return this.waitTime;
+		}
+		
+		public void setWaitTime(long waitTime) {
+			this.waitTime = waitTime;
+		}
 	}
 }

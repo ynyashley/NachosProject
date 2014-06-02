d14f47fcf495ac1a97129995391d20f0a00a1227
Ashley Yung <nyung@ieng6-202.ucsd.edu>
2014-06-02 15:52:38 -0700

    working on part 2

--- vm/VMKernel.java
+++ vm/VMKernel.java
@@ -5,6 +5,8 @@ import nachos.threads.*;
 import nachos.userprog.*;
 import nachos.vm.*;
 
+import java.util.*;
+
 /**
  * A kernel that can support multiple demand-paging user processes.
  */
@@ -14,6 +16,10 @@ public class VMKernel extends UserKernel {
 	 */
 	public VMKernel() {
 		super();
+		ipt = new PageTableEntryInfo[Machine.processor().getNumPhysPages()];
+		swapFile = ThreadedKernel.fileSystem.open("swapFile", true);
+		freeSwapPages = new LinkedList<Boolean>();
+		iptLock = new Lock();
 	}
 
 	/**
@@ -43,9 +49,60 @@ public class VMKernel extends UserKernel {
 	public void terminate() {
 		super.terminate();
 	}
-
+	
+	public class PageTableEntryInfo {
+		private int processID;
+		private TranslationEntry entry;
+		private int pinCount;
+		private boolean freeWhenUnpinned;
+		
+		public int getProcessID() {
+			return processID;
+		}
+		public void setProcessID(int processID) {
+			this.processID = processID;
+		}
+		public TranslationEntry getEntry() {
+			return entry;
+		}
+		public void setEntry(TranslationEntry entry) {
+			this.entry = entry;
+		}
+		public int getPinCount() {
+			return pinCount;
+		}
+		public void setPinCount(int pinCount) {
+			this.pinCount = pinCount;
+		}
+		public boolean isFreeWhenUnpinned() {
+			return freeWhenUnpinned;
+		}
+		public void setFreeWhenUnpinned(boolean freeWhenUnpinned) {
+			this.freeWhenUnpinned = freeWhenUnpinned;
+		}
+	}
+	
+	public static OpenFile swapFile;
+	
+	public static LinkedList<Boolean> freeSwapPages;
+	
+	public static PageTableEntryInfo[] ipt;
+	
+	public static Lock iptLock;
 	// dummy variables to make javac smarter
 	private static VMProcess dummy1 = null;
 
 	private static final char dbgVM = 'v';
+
+	public static void pinPage(int ppn) {
+		iptLock.acquire();
+		ipt[ppn].setPinCount(++ipt[ppn].pinCount);
+		iptLock.release();
+	}
+
+	public static void unpinPage(int ppn) {
+		iptLock.acquire();
+		ipt[ppn].setPinCount(--ipt[ppn].pinCount);
+		iptLock.release();
+	}
 }
--- vm/VMProcess.java
+++ vm/VMProcess.java
@@ -4,6 +4,7 @@ import nachos.machine.*;
 import nachos.threads.*;
 import nachos.userprog.*;
 import nachos.vm.*;
+import nachos.vm.VMKernel.PageTableEntryInfo;
 
 /**
  * A <tt>UserProcess</tt> that supports demand-paging.
@@ -21,7 +22,22 @@ public class VMProcess extends UserProcess {
 	 * Called by <tt>UThread.saveState()</tt>.
 	 */
 	public void saveState() {
-		super.saveState();
+		// TODO: add pagetable lock
+		/*
+		 * Flush TLB on context switch
+		 */
+		for (int i = 0; i < Machine.processor().getTLBSize(); i++) {
+			TranslationEntry entry = Machine.processor().readTLBEntry(i);
+			// sync entry with page table if entry is valid
+			if (entry.valid) {
+				pageTable[entry.vpn] = new TranslationEntry(entry.vpn,
+						entry.ppn, entry.valid, entry.readOnly, entry.used,
+						entry.dirty);
+			}
+			// invalidate entry
+			entry.valid = false;
+			Machine.processor().writeTLBEntry(i, entry);
+		}
 	}
 
 	/**
@@ -29,7 +45,7 @@ public class VMProcess extends UserProcess {
 	 * <tt>UThread.restoreState()</tt>.
 	 */
 	public void restoreState() {
-		//super.restoreState();
+		super.restoreState();
 	}
 
 	/**
@@ -72,7 +88,6 @@ public class VMProcess extends UserProcess {
 
 	private void handleTLBMiss() {
 		// Get VPN
-		//System.out.println("In handleTLBMiss");
 		int virtualAddress = Machine.processor().readRegister(
 				Processor.regBadVAddr);
 		int vpn = Processor.pageFromAddress(virtualAddress);
@@ -90,9 +105,10 @@ public class VMProcess extends UserProcess {
 				}
 			}
 			if (!invalidEntryFound) {
-				// evict a victim
+				// randomly evict a victim
 				int victimIndex = Lib.random(Machine.processor().getTLBSize());
-				TranslationEntry victim = Machine.processor().readTLBEntry(victimIndex);
+				TranslationEntry victim = Machine.processor().readTLBEntry(
+						victimIndex);
 				// sync entry with page table
 				pageTable[victim.vpn] = new TranslationEntry(victim.vpn,
 						victim.ppn, victim.valid, victim.readOnly, victim.used,
@@ -101,8 +117,100 @@ public class VMProcess extends UserProcess {
 				Machine.processor().writeTLBEntry(victimIndex, entry);
 			}
 		} else { // page fault
-
+			handlePageFault(entry);
 		}
+	}
+
+	private void handlePageFault(TranslationEntry entry) {
+		int ppn = allocatePhysicalPage(entry);
+		if (entry.dirty) { // swap in
+			int index = entry.ppn;
+			entry.ppn = ppn;
+			// TODO: may not pinPage
+			VMKernel.pinPage(ppn);
+			entry.valid = true;
+			VMKernel.swapFile.read(index * pageSize, Machine.processor()
+					.getMemory(), entry.ppn * pageSize, pageSize);
+			VMKernel.freeSwapPages.set(index, false);
+			VMKernel.unpinPage(ppn);
+		} else {
+			// TODO: Not sure if our code is right
+			entry.ppn = ppn;
+			VMKernel.pinPage(ppn);
+			entry.valid = true;
+			CoffSection section = coff.getSection(entry.vpn);
+			section.loadPage((entry.vpn - section.getFirstVPN()), ppn);
+			VMKernel.unpinPage(ppn);
+		}
+		// sync page table and inverted page table
+		pageTable[entry.vpn] = entry;
+		VMKernel.ipt[ppn].setProcessID(processID());
+		VMKernel.ipt[ppn].setEntry(entry);
+	}
+
+	private int allocatePhysicalPage(TranslationEntry entry) {
+		int ppn = 0;
+		if (UserKernel.freePages.isEmpty()) { // no free memory, need to evict a
+												// page
+			// TODO: sync TLB entries
+			ppn = clockAlgorithm(); // select a victim for replacement
+
+		} else { // allocate a free page from list
+			ppn = ((Integer) UserKernel.freePages.removeFirst()).intValue();
+			// TODO: just keep the old values of the other bits?
+			pageTable[entry.vpn] = new TranslationEntry(entry.vpn, ppn,
+					entry.valid, entry.readOnly, entry.used, entry.dirty);
+		}
+		return ppn;
+	}
+
+	private int clockAlgorithm() {
+		int clockHand = 0;
+		TranslationEntry victim = null;
+		while (UserKernel.freePages.isEmpty()) {
+			PageTableEntryInfo frame = VMKernel.ipt[clockHand];
+			if (frame.getPinCount() < 1) { // swap
+				if (frame.getEntry().used) {
+					frame.getEntry().used = false;
+				} else {
+					victim = frame.getEntry();
+					if (victim.dirty) { // swap the page out
+						int index = assignSwapSpace();
+						// byte [] memory = new byte[pageSize];
+						// TODO: not sure about victim.vpn*pageSize
+						// readVirtualMemory(victim.vpn*pageSize, memory);
+						VMKernel.swapFile.write(index * pageSize, Machine
+								.processor().getMemory(),
+								victim.ppn * pageSize, pageSize);
+						victim.ppn = index;
+					}
+					UserKernel.freePages.add(victim.ppn);
+					victim.valid = false; // invalidate PTE
+					pageTable[victim.vpn].valid = false;
+					// Invalidate TLB entry if it is there
+					for (int i = 0; i < Machine.processor().getTLBSize(); i++) {
+						int vpn = Machine.processor().readTLBEntry(i).vpn;
+						if (victim.vpn == vpn) {
+							Machine.processor().writeTLBEntry(i, victim);
+							break;
+						}
+					}
+				}
+			}
+			clockHand = (clockHand + 1) % Machine.processor().getNumPhysPages();
+		}
+		return victim.ppn;
+	}
+
+	private int assignSwapSpace() {
+		for (int i = 0; i < VMKernel.freeSwapPages.size(); i++) {
+			if (VMKernel.freeSwapPages.get(i) == false) {
+				VMKernel.freeSwapPages.set(i, true);
+				return i;
+			}
+		}
+		VMKernel.freeSwapPages.add(new Boolean(true));
+		return (VMKernel.freeSwapPages.size() - 1);
 	}
 
 	private static final int pageSize = Processor.pageSize;
